a7c3292aa4b7f1124c7ab681ecc9b9cabc08302e
goutam-patidar-28 <141953869+goutam-patidar-28@users.noreply.github.com>
2025-08-04 00:49:04 +0530

    Delete handsOn11 directory

--- handsOn11/Code/eventexamplesapp/src/components/WelcomeButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function WelcomeButton() {
-  const sayWelcome = (msg) => alert(msg);
-
-  return <button onClick={() => sayWelcome("welcome")}>Say Welcome</button>;
-}
-
-export default WelcomeButton;
